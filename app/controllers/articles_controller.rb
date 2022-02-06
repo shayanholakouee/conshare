dd3926a40669ae691d9fdf028537c98505b2fb39
shayan holakouee <shayanholakuee@gmail.com>
2022-02-06 07:12:19 +0400

    latest modifivations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,9 +36,17 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "article deleted"
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :description)
   end
+
 end
