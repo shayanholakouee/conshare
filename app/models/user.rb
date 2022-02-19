4c731777dffb5aae99a8877271171913c791fdf9
shayan holakouee <shayanholakuee@gmail.com>
2022-02-19 19:04:58 +0400

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :username,
+            presence: true,
+            length: {maximum: 5, minimum: 25},
+            uniqueness: { case_sensitive: false}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+            presence: true,
+            length: {maximum: 50},
+            uniqueness: {case_sensitive: false},
+            format: {with: VALID_EMAIL_REGEX}
 end
